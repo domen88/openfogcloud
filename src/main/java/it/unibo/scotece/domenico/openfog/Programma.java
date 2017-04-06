672289b0859366faf6e9fe779790682f2fb97178
domen88 <domen.88@live.it>
2017-04-06 18:15:53 +0200

    Create container from image

--- src/main/java/it/unibo/scotece/domenico/openfog/Programma.java
+++ src/main/java/it/unibo/scotece/domenico/openfog/Programma.java
@@ -2,15 +2,19 @@ package it.unibo.scotece.domenico.openfog;
 
 import static spark.Spark.*;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.spotify.docker.client.DefaultDockerClient;
 import com.spotify.docker.client.DockerCertificates;
 import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.exceptions.DockerCertificateException;
 import com.spotify.docker.client.exceptions.DockerException;
-import com.spotify.docker.client.messages.Info;
+import com.spotify.docker.client.messages.*;
 
 import java.net.URI;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 
 
 public class Programma {
@@ -27,8 +31,36 @@ public class Programma {
                     .build();
 
             final Info info = docker.info();
-            System.out.print(info);
+            System.out.println(info);
 
+            // By pulling
+            final RegistryAuth registryAuth = RegistryAuth.builder()
+                    .email("EMAIL")
+                    .username("USER")
+                    .password("PWD")
+                    .build();
+            docker.pull("domen88/hello:latest", registryAuth);
+
+            //Container MAP PORT
+            Map<String, List<PortBinding>> portBindings = Maps.newHashMap();
+            // you can leave the host IP empty for the PortBinding.of first parameter
+            portBindings.put("80/tcp", Lists.newArrayList(PortBinding.of("", "8090")));
+            HostConfig hostConfig = HostConfig.builder()
+                    .portBindings(portBindings)
+                    // other host config here
+                    .build();
+
+            //Create Container
+            final ContainerCreation container = docker.createContainer(
+                    ContainerConfig.builder()
+                            .image("domen88/hello")
+                            .hostConfig(hostConfig)
+                            .build());
+
+            //Start Container
+            docker.startContainer(container.id());
+
+            System.out.println(container.toString());
             // Close the docker client
             docker.close();
 
